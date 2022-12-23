a884c494885aa94f830d8438eac6b7945b7d3082
FKD13 <francisklinck@gmail.com>
2022-12-23 19:12:10 +0100

    add bundle script

---
+++ make_submission.sh
@@ -0,0 +1,14 @@
+rm -f submission.zip
+
+zip submission.zip \
+	data.zip \
+	data_reimport.zip \
+	1.3.viterbi_lapper.ipynb \
+	3.1.fitting_the_data.ipynb \
+	3.2.improving_the_data.ipynb \
+	README.md \
+	.tool-versions \
+	requirements.txt \
+	static_probabilities.py \
+	report.pdf \
+	HMM.java
